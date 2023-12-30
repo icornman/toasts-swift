0697c613b9d7490dec63f231858780d24b748b89
Vlad Ishchuk <vlad.ischuck2006@gmail.com>
2023-12-30 11:16:30 +0200

    Initial Commit

---
+++ toasts/toastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  toastsApp.swift
+//  toasts
+//
+//  Created by Vlad Ishchuk on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct toastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
