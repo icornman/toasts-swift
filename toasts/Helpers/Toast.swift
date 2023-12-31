7f9aabc6a19fb4e57b3d6e5e87de864e81ba2f4d
Vlad Ishchuk <vlad.ischuck2006@gmail.com>
2024-01-01 01:30:46 +0200

    Add demo toast

---
+++ toasts/Helpers/Toast.swift
@@ -0,0 +1,18 @@
+//
+//  Toast.swift
+//  toasts
+//
+//  Created by Vlad Ishchuk on 30.12.2023.
+//
+
+import SwiftUI
+
+struct Toast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toast()
+}
